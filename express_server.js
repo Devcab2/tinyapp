703b54ec584f0b7376665eea8b893434cfa11234
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-07-14 11:20:21 -0600

    implemented id shortener and redirect request

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 8080; //default
 app.set("view engine", "ejs");
 
-function generateRandomString() {
+const generateRandomString = function() {
   const uniqueId = Math.random().toString(36).substr(2, 6);
   return uniqueId;
 };
@@ -40,14 +40,19 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  let id = generateRandomString();
+  urlDatabase[id] = req.body.longURL;
+  res.redirect(`/urls/${id}`);
+});
+
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);
-  res.send("Ok");
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
-
-
