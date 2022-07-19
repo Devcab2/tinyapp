a6649c3789543dda9a895134ee062799c2664466
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-07-19 15:41:39 -0600

    add helper function descriptions

--- helper_function.js
+++ helper_function.js
@@ -1,5 +1,11 @@
 const {urlDatabase,} = require('./objects.js');
 
+/* finds user id
+*  compares email to users object
+*  if the email exists, returns x user
+*  else null
+*/
+
 const getUserByEmail = (email, users) => {
   for (const userId in users) {
     if (users[userId].email === email) {
@@ -9,10 +15,19 @@ const getUserByEmail = (email, users) => {
   return null;
 };
 
+/*  6 digit random Alphanumeric number generator
+    returns our new uniqueId
+*/
+
 const generateRandomString = function() {
   const uniqueId = Math.random().toString(36).substr(2, 6);
   return uniqueId;
 };
+
+/* Get all urls for user
+*  @params id: string
+*  @returns urldatabase[]
+*/
 const urlsForUser = (id) => {
   const urls = {};
   for (let key in urlDatabase) {
